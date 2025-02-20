5dde147b69a7c107da2e087e93bae562254116d5
feederndt <nd,thang141020@gmail.com>
2025-02-20 22:09:55 +0700

    a

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
 import { SuiClient, getFullnodeUrl } from "@mysten/sui/client";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import { Transaction } from "@mysten/sui/transactions";
+import fs from "fs"
 
-const PRIVATE_KEY =
-  "suiprivkey1qz69qpql7l2khrrw6jvsvhr82pyx89gtzl5lrdyvvplrfw203dl8jtv7juw";
+const address_datas = fs.readFileSync('walletAddress.txt', 'utf8');
 
-const RECIPIENT_ADDRESS =
-  "0x72636c6bfbe3a2b3ae487802fcd4ba548bc63065eb04adaa08cdea97e76d568b";
+const private_datas = fs.readFileSync('privateKey.txt', 'utf8');
 
-const AMOUNT = 0.01;
+
+const walletArr = address_datas.split(/\r?\n|\r|\n/g).filter(e => e.length > 0)
+
+const privateArr = private_datas.split(/\r?\n|\r|\n/g).filter(e => e.length > 0)
+
+const GAS_BUDGET = 1500000
 
 const client = new SuiClient({ url: getFullnodeUrl("mainnet") });
 
-const keypair = Ed25519Keypair.fromSecretKey(PRIVATE_KEY);
-
-console.log(MIST_PER_SUI);
 
 async function sendSui() {
-  const tx = new Transaction();
-  const [coin] = tx.splitCoins(tx.gas, [100000000]);
-  tx.transferObjects([coin], RECIPIENT_ADDRESS);
-  tx.setGasBudget(0.0015 * 10 ** 9);
 
-  const result = await client.signAndExecuteTransaction({
-    signer: keypair,
-    transaction: tx,
-  });
+  if (walletArr.length != privateArr.length || walletArr.length == 0 || privateArr.length) {
+    console.log("Dlm nhìn lại")
+  } else {
+    for (let i = 0; i < walletArr.length; i++) {
+      const keypair = Ed25519Keypair.fromSecretKey(privateArr[i]);
 
-  return result;
+      const senderAddress = keypair.getPublicKey().toSuiAddress()
+
+      const coins = await client.getCoins({
+        owner: senderAddress,
+      });
+
+      const suiBalance = Number(coins.data[0].balance)
+      const numTrans = suiBalance - GAS_BUDGET
+
+      const tx = new Transaction();
+      const [coin] = tx.splitCoins(tx.gas, [numTrans]);
+      tx.transferObjects([coin], walletArr[i]);
+      tx.setGasBudget(GAS_BUDGET);
+
+      const result = await client.signAndExecuteTransaction({
+        signer: keypair,
+        transaction: tx,
+      })
+
+      result.then((result) => {
+        console.log("Transaction Digest:", result.digest);
+      })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  }
 }
 
 sendSui()
-  .then((result) => {
-    console.log("Transaction Digest:", result.digest);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+
